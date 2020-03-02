fa833582e85f0306e03b88be5da24be72bec43d0
Rouger Baptiste <contact@skill-up.fr>
2020-03-02 12:01:42 +0100

    Initial Commit

---
+++ First Aid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  First Aid
+//
+//  Created by Dev iOS on 02/03/2020.
+//  Copyright © 2020 Captain301. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
